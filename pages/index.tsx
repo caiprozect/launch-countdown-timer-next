607a64b472a761898ea603eae77cc59077753594
Caiprozect <caiprozect@gmail.com>
2022-02-08 00:11:46 +0900

    Base for flip card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,54 @@
 import Head from 'next/head'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Home() {
+  const [cur, setCur] = useState(9)
+  const [next, setNext] = useState(8)
+
+  useEffect(() => {
+    const counter = setInterval(() => {
+      if (next === 0) {
+        clearInterval(counter)
+        return
+      }
+
+      setCur(cur - 1)
+      setNext(next - 1)
+    }, 1000)
+    return () => clearInterval(counter)
+  }, [cur, next])
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Countdown timer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+      <main className="min-w-screen threeD flex min-h-screen items-center justify-center">
+        <div
+          key={cur}
+          className="flippable flipX relative h-20 w-40 bg-transparent"
+        >
+          <div className="absolute h-full w-full overflow-hidden bg-blue-500 text-center text-9xl">
+            <p className="mt-2">{cur}</p>
+          </div>
+          <div
+            className="absolute h-full w-full overflow-hidden bg-red-500 text-center text-9xl"
+            style={{
+              transform: 'rotateX(180deg)',
+            }}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <p className="mt-2 -translate-y-20">{next}</p>
+          </div>
+        </div>
+        <div className="absolute -z-10 h-20 w-40 overflow-hidden bg-red-200 text-center text-9xl">
+          <p className="mt-2">{next}</p>
+        </div>
+        <div className="absolute -z-10 h-20 w-40 translate-y-20 overflow-hidden bg-blue-200 text-center text-9xl">
+          <p className="mt-2 -translate-y-20">{cur}</p>
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
-      </footer>
     </div>
   )
 }
